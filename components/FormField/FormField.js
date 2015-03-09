c57e36ed1f66e5f552e10dca2a31154ad03860ac
Aleksey Verkholantsev <alex-vee@yandex-team.ru>
2015-03-09 17:09:37 +0300

    FormField works with types

--- components/FormField/FormField.js
+++ components/FormField/FormField.js
@@ -2,16 +2,31 @@
 
 import React from 'react';
 import Input from '../Input/Input';
+import Select from '../Select/Select';
 
 export default class FormField extends React.Component {
+    getComponentConstructor(type) {
+        switch (type) {
+        case 'Input':
+            return Input;
+        case 'Select':
+            return Select;
+        default:
+            throw new Error(`Unknown type ${type}`);
+        }
+    }
+
     render() {
+        let type = this.props.type;
+        let Component = this.getComponentConstructor(type);
+
         return (
             <div className='form-field'>
                 <div className='form-field__label'>
                     {this.props.label}
                 </div>
                 <div className='form-field__control'>
-                    <Input
+                    <Component
                         hint={this.props.hint}
                         actionChanged={this.props.actionChanged}
                     />
---
+++ components/Select/Select.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import React from 'react';
+
+export default class Select extends React.Component {
+    render() {
+        return (<div>Select</div>);
+    }
+}
